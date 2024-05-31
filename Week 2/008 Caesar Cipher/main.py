b0eea09cea51b43ffee2b6a43c7ed5dcfb6f36d0
vehehgito <aryann2708@gmail.com>
2024-05-31 19:48:17 +0530

    Day 010 of 100 days of code completed.

---
+++ Week 2/008 Caesar Cipher/main.py
@@ -0,0 +1,51 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+	end_text = ""
+
+	if cipher_direction == "2":
+		shift_amount *= -1
+
+	for char in start_text:
+		if char in alphabet:
+			position = alphabet.index(char)
+			new_position = position + shift_amount
+			end_text += alphabet[new_position]
+		else:
+			end_text += char
+
+	print(f"Result: {end_text}")
+
+print('''        
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+''')
+
+should_end = False
+
+while not should_end:
+	direction = input("Type '1' to encrypt, type '2' to decrypt:\n")
+	text = input("Type your message:\n").lower()
+	shift = int(input("Type the shift number:\n"))
+	shift = shift % 26
+
+	caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+	restart = input("Type 'q' to exit.\n")
+
+	if restart == "q":
+		should_end = True
+		print("Goodbye")
